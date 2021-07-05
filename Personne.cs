be35edbcac045df2741a1dc5cd755940b9414d90
Adoua Ange <Adoua Ange@DESKTOP-NK61TD3>
2021-07-05 00:52:09 -0400

    Ajoutez des fichiers projet.

---
+++ Personne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Basis
+{
+	public class Personne
+	{
+		public string _nom;
+		public string _prenom;
+		public int _age;
+
+		public Personne(string nom)
+		{
+			_nom = nom;
+		}
+
+		public Personne(string nom, string prenom) : this(nom)
+		{
+			_prenom = prenom;
+		}
+
+		public Personne(string nom, string prenom, int age) : this(nom, prenom)
+		{
+			_age = age;
+		}
+
+		public void AfficherInfo()
+		{
+			Console.WriteLine("Nom: {0}", _nom);
+			Console.WriteLine("Prenom: {0}", _prenom);
+			Console.WriteLine("Age: {0}", _age);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Basis
+{
+	class Program
+	{
+		static void Main(string[] args) 
+		{
+			var toto = new Personne("Toto", "Tata", 20);
+			toto.AfficherInfo();
+		}
+
+	}
+}
