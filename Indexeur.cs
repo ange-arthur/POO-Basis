02f9d0f186c624ff32639c66d38659677bcfa8b3
Adoua Ange <adoua.arthur@gmail.com>
2021-07-07 06:05:36 -0400

    Ajout de nouvelles classes

---
+++ Indexeur.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Basis
+{
+    class Indexeur
+    {
+        public readonly Dictionary<string, string> _listeCompteBancaire;
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        public Indexeur()
+        {
+            _listeCompteBancaire = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return _listeCompteBancaire[key];
+            }
+
+            set
+            {
+                _listeCompteBancaire[key] = value;
+            }
+        }
+    }
+}
---
+++ Properties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Basis
+{
+    class Properties
+    {
+        public readonly DateTime _dateDeNaissance;
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get 
+            {
+                var ageref = DateTime.Now - _dateDeNaissance;
+                return (ageref.Days / 365);
+            } 
+            private set { }
+        }
+
+        public Properties(DateTime DateNaissance)
+        {
+            _dateDeNaissance = DateNaissance;
+        }
+
+        public void AfficherInfo()
+        {
+            Console.WriteLine("Nom : {0}", Nom);
+            Console.WriteLine("Prenom : {0}", Prenom);
+            Console.WriteLine("Date de naissance : {0}", _dateDeNaissance.ToShortDateString());
+            Console.WriteLine("Age : {0}", Age);
+
+        }
+    }
+}
